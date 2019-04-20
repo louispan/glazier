fb711663c00ffc0add7b3d07dc395db2052270bc
Louis Pan <louis@pan.me>
2019-04-21 04:22:05 +1000

    Only NonBlocking needs to be protected

--- src/Glazier/Command/Internal.hs
+++ src/Glazier/Command/Internal.hs
@@ -9,12 +9,6 @@ import Control.Concurrent.STM
 newtype NonBlocking m a = NonBlocking (m a)
     deriving (Functor, Applicative, Monad)
 
--- The Cargo is something than can only be read from once
--- The read is blocks until it is marked ready to be read by another thread.
--- The sending thread never blocks, but you can't send (does nothing)
--- after the bus is consumed.
--- data Cargo a = CargoConsumed | CargoReady (Chan a) | CargoConsolidating (Chan a)
-
 unNonBlocking :: NonBlocking m a -> m a
 unNonBlocking (NonBlocking m) = m
 
